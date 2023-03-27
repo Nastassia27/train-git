10819cac5c39277edcbc1e6c7494db70a438a13b
Nastassia27 <anastasia.derugo@gmail.com>
2023-03-27 21:51:46 +0300

    create tests

---
+++ src/01-hello-tests/01.test.ts
@@ -0,0 +1,81 @@
+import {mult, splitIntoWords, sum} from "./01";
+
+
+let a: number;
+let b: number;
+let c: number;
+beforeEach(()=>{
+    a=1;
+    b=2;
+    c=3;
+})
+
+test('sum should be correct', ()=>{
+   // data
+
+
+    //action
+    const result = sum(a,b);
+
+    //result
+
+    expect(result).toBe(3);
+
+})
+
+test('sum2 should be correct', ()=>{
+    // data
+
+
+    //action
+    const result1 = sum(a,b);
+    const result2 = sum(c,b);
+
+    //result
+
+    expect(result1).toBe(3);
+    expect(result2).toBe(5);
+
+})
+
+test('mult should be correct', ()=>{
+    // data
+    const a = 1;
+    const b = 2;
+    const c = 3;
+
+    //action
+    const mult1 = mult(a,b);
+    const mult2 = mult(b,c);
+
+    //result
+
+    expect(mult1).toBe(2);
+    expect(mult2).toBe(6);
+
+})
+
+test('split shpuld be correct', ()=>{
+    // data
+    const sent1 = 'Hello my friends';
+    const sent2 = 'js-language.';
+
+
+    //action
+    const mult1 = splitIntoWords(sent1);
+    const mult2 = splitIntoWords(sent1);
+
+    // expectresult
+
+    expect(mult1.length).toBe(3);
+    expect(mult1[0]).toBe('hello');
+    expect(mult1[1]).toBe('my');
+    expect(mult1[2]).toBe('friend');
+
+
+    expect(mult2.length).toBe(3);
+    expect(mult2[0]).toBe('hello');
+    expect(mult2[1]).toBe('my');
+    expect(mult2[2]).toBe('friend');
+
+})
